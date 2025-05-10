506fb7428c26956e6e36b377aac4b0aee2206528
Diegxta <diego.cordobagonzalez@gmail.com>
2025-05-10 18:48:07 +0200

    GameLoop

--- Assets/Art/Scripts/PickAndDropManager.cs
+++ Assets/Art/Scripts/PickAndDropManager.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PickAndDropManager : MonoBehaviour
 {
     private int conteo;
     [SerializeField]
     private int conteoDeseado;
-    
+    private GameObject player;
+
     public void avanceNivel()
     {
         conteo++;
-        if (conteo >= conteoDeseado)
+        //Si estamos en la fase de negación, invertimos los controles again
+        if(conteo >= conteoDeseado)
         {
             //TODO cambio de escena
             Debug.Log("TODO cambio de escena");
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            player = GameObject.FindWithTag("Player");
+
+            player.GetComponent<PlayerController>().invertControls = !player.GetComponent<PlayerController>().invertControls;
         }
     }
 }
