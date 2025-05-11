97c423099a49b4377304c3dc1daf417b5d4f3bfd
flyeers <juliafluper@gmail.com>
2025-05-11 10:26:54 +0200

    reset day

--- Assets/Scripts/Trampitas.cs
+++ Assets/Scripts/Trampitas.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Trampitas : MonoBehaviour
 {
     private KeyCode keyToDetect = KeyCode.P;
+    private KeyCode keyToDetect2 = KeyCode.O;
 
     void Update()
     {
@@ -10,6 +12,11 @@ public class Trampitas : MonoBehaviour
         {
             GameObject sceneManager = GameObject.Find("SceneManager");
             if (sceneManager) sceneManager.GetComponent<SceneController2>().TrampitaNextLevel();
+        }
+
+        if (Input.GetKeyDown(keyToDetect2))
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
