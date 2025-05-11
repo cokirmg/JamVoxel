d4418e84b50be876d988bc188257c827e08a9c7a
Diegxta <diego.cordobagonzalez@gmail.com>
2025-05-11 07:29:29 +0200

    Cosas

--- Assets/Art/Scripts/SceneController2.cs
+++ Assets/Art/Scripts/SceneController2.cs
@@ -56,30 +56,12 @@ public class SceneController2 : MonoBehaviour
         animator.SetTrigger("End");
         yield return new WaitForSeconds(2);
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
-        animator.SetTrigger("Start");        
-    }
-
-    private void Update()
-    {
-        if (SceneManager.GetActiveScene().buildIndex == 4 && !fade1)
+        animator.SetTrigger("Start");
+        if (SceneManager.GetActiveScene().buildIndex +1  == 4)
         {
-            audioSource.volume = 0;
             audioSource.clip = audioClip;
             audioSource.Play();
-            audioSource.volume = Mathf.Lerp(0, 0.3f, 0.1f);
-            fade1 = true;
-        }
-        else if (SceneManager.GetActiveScene().buildIndex == 5 && !fade2)
-        {
-            audioSource.volume = Mathf.Lerp(0.3f, 0.6f, 0.1f);
-            fade2 = true;
-        }
-        else if (SceneManager.GetActiveScene().buildIndex == 6 && !fade3)
-        {
-            audioSource.volume = Mathf.Lerp(0.6f, 0.9f, 0.1f);
 
-            fade3 = true;
         }
     }
-
 }
