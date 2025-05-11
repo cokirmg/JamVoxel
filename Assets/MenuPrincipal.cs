14a5beb2507976555bc7a3c9a2c940dda35bd203
Diegxta <diego.cordobagonzalez@gmail.com>
2025-05-11 09:42:13 +0200

    PrincipalLvl

--- Assets/MenuPrincipal.cs
+++ Assets/MenuPrincipal.cs
@@ -5,12 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
-    
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+    private GameObject FadeOut;
+    [SerializeField] Animator animator;
     public void Jugar()
     {
-        SceneManager.LoadScene(1);
+        FadeOut = GameObject.Find("FadeOut");
+        if(FadeOut)
+        {
+            FadeOut.SetActive(true);
+
+            //yield return new WaitForSeconds(2);
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            //yield return new WaitForSeconds(2);
+            StartCoroutine(StopAt());
+        }
     }
 
+    private IEnumerator StopAt()
+    {
+        yield return new WaitForSeconds(5.5f);
+        SceneManager.LoadScene(1);
+    }
     public void Salir()
     {
         Debug.Log("Salir");
